d39d9142db940d88e485edd071000676af053e30
amsko <amskho78@gmail.com>
2024-05-22 10:24:24 +0200

    ajout de l'ignore

---
+++ profil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="" />
+        <label class="form-check-label" for=""> Default checkbox </label>
+    </div>
+    <div class="form-check">
+        <input
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id=""
+            checked
+        />
+        <label class="form-check-label" for=""> Checked checkbox </label>
+    </div>
+    
+</body>
+</html>
